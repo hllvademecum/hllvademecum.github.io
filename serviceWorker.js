95f7a4394ee911b1553d28403cd0a76135dad564
hllvademecum <164743595+hllvademecum@users.noreply.github.com>
2024-03-29 07:02:10 +0100

    pwa_auto_update

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticHLLVadeMecum = "hll-vade-mecum-v1.3"
+const staticHLLVadeMecum = "hll-vade-mecum-v1.4"; // Update the cache version when changes are made
 const assets = [
   "/",
   "/index.html",
@@ -6,20 +6,45 @@ const assets = [
   "/js/app.js",
   "/images/about.png",
   "/images/hll_arty.png",
-]
+];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticHLLVadeMecum).then(cache => {
-      cache.addAll(assets)
+      return cache.addAll(assets); // Return the promise chain to properly handle errors
     })
-  )
-})
+  );
+});
+
+self.addEventListener("activate", activateEvent => {
+  activateEvent.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          // Delete outdated caches
+          return cacheName.startsWith("hll-vade-mecum-") && cacheName !== staticHLLVadeMecum;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
+    caches.match(fetchEvent.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse; // Return cached response if found
+      }
+
+      return fetch(fetchEvent.request).then(networkResponse => {
+        // Cache and return network response
+        return caches.open(staticHLLVadeMecum).then(cache => {
+          cache.put(fetchEvent.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
     })
-  )
-})
+  );
+});
